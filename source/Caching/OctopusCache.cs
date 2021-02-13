c016252e6a7dbde148ec2e850f1e00c5e084fad5
Matt Richardson <matt.richardson@octopus.com>
2021-02-13 16:59:37 +1100

    Added support for task based (async) value factories
    Previously, if an exception was thrown, it was cache a null response

--- source/Caching/OctopusCache.cs
+++ source/Caching/OctopusCache.cs
@@ -32,6 +32,22 @@ namespace Octopus.Caching
             }
         }
 
+        public async Task<TItem> GetOrAdd<TItem>(string key, Func<Task<TItem>> valueFactory, TimeSpan expiresIn)
+            where TItem : notnull
+        {
+            try
+            {
+                return await (Task<TItem>) GetOrAddEntry(key, valueFactory, expiresIn).Item.Value;
+            }
+            catch
+            {
+                // Lazy initialization failed, we don't want to cache the exception
+                // This could possibly evict a successful resolution that happened in the meantime, but that's not too terrible
+                Delete(key);
+                throw;
+            }
+        }
+
         Entry GetOrAddEntry<TItem>(string key, Func<TItem> valueFactory, TimeSpan expiresIn)
             where TItem : notnull
         {
